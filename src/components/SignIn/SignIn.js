576ec9c4766e8ccd450ca7cf8327aa3f5724b5ac
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-27 14:16:48 +1000

    Add SignIn

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,11 @@
+import style from "./SignIn.module.css";
+
+function SignIn() {
+  return (
+    <div className={style.container}>
+      <button>Sign In with Google</button>
+    </div>
+  );
+}
+
+export default SignIn;
