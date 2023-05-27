b679a71342caa1f6ffc9bec84f6093292f944863
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-27 13:02:06 +1000

    Add Feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import Header from "./components/header/Header";
+import Feed from "./components/Feed/Feed";
 
 function App() {
   return (
     <div className={style.App}>
       <Header />
+      <Feed />
     </div>
   );
 }
---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,7 @@
+import style from "./Feed.module.css";
+
+function Feed() {
+  return <div className={style.container}></div>;
+}
+
+export default Feed;
