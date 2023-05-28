be72ac99aa8350af2c57fb9a903a6b3545e6988c
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-28 12:46:48 +1000

    Add header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,26 @@ import style from "./Header.module.css";
 function Header({ auth }) {
   return (
     <div className={style.container}>
-      <h1>MSN Chat</h1>
+      <div className={style.headerTop}>
+        <div className={style.title}>
+          <div className={style.titleIcon}>
+            <img src="https://picsum.photos/15/15" />
+          </div>
+          <h4>MSN Chat</h4>
+        </div>
+        <div className={style.windowBtns}>
+          <button>_</button>
+          <button>□</button>
+          <button>x</button>
+        </div>
+      </div>
+      <div className={style.headerBottom}>
+        <button>File</button>
+        <button>Contacts</button>
+        <button>Actions</button>
+        <button>Tools</button>
+        <button>Help</button>
+      </div>
       {auth.currentUser && (
         <button onClick={() => auth.signOut()}>Sign out</button>
       )}
