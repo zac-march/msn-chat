9b9d41fa0b30969e80a46b8639677664edb60f96
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-28 16:37:06 +1000

    Host with firebase

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -3,7 +3,6 @@ import style from "./Message.module.css";
 function Message({ msg, auth }) {
   const { currentUser: user } = auth;
   const messageStyle = msg.uid === user.uid ? style.sent : style.recieved;
-  console.log(auth.currentUser);
   return (
     <div className={`${messageStyle} ${style.message}`}>
       <p className={style.sender}>{msg.senderName} says:</p>
